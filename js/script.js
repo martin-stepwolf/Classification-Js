275d4d8cb93e26b300f023abf69970cc3b27d7f1
IngCampos <15151130@mail.ita.mx>
2019-04-01 18:33:56 -0600

    Lectura archivo csv
    Los muestra en una tabla

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 ﻿$(document).ready(function(){
-
+    $.ajax({
+        url: 'Datos recabados - Hoja 1.csv',
+        dataType: 'text',
+      }).done(successFunction);
+      function successFunction(data){
+        var allRows = data.split(/\r?\n|\r/);
+        var table = '<table border=1px>';
+        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+          if (singleRow === 0) {
+            table += '<thead>';
+            table += '<tr>';
+          } else {
+            table += '<tr>';
+          }
+          var rowCells = allRows[singleRow].split(',');
+          for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
+            if (singleRow === 0) {
+              table += '<th>';
+              table += rowCells[rowCell];
+              table += '</th>';
+            } else {
+              table += '<td>';
+              table += rowCells[rowCell];
+              table += '</td>';
+            }
+          }
+          if (singleRow === 0) {
+            table += '</tr>';
+            table += '</thead>';
+            table += '<tbody>';
+          } else {
+            table += '</tr>';
+          }
+        } 
+        table += '</tbody>';
+        table += '</table>';
+        $('body').append(table);  
+    }
 });
