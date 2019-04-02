7507852f482990e8fe4e4c82ff7d604d6613fbcd
IngCampos <15151130@mail.ita.mx>
2019-04-02 08:25:34 -0600

    Mayusculas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-﻿$(document).ready(function(){
-    $.ajax({
+﻿function Lectura(){
+  $.ajax({
         url: 'Datos recabados - Hoja 1.csv',
         dataType: 'text',
       }).done(successFunction);
       function successFunction(data){
+        data = data.toUpperCase();
+        data = data.replace(/['"]+/g, '');
+        console.log(data);
+        
+        
         var allRows = data.split(/\r?\n|\r/);
+        console.log(allRows.length);
         var table = '<table border=1px>';
         for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
           if (singleRow === 0) {
@@ -13,7 +19,10 @@
           } else {
             table += '<tr>';
           }
+
           var rowCells = allRows[singleRow].split(',');
+          let sum = rowCells.reduce((previous, current) => current += previous);
+          let avg = sum / rowCells.length;
           for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
             if (singleRow === 0) {
               table += '<th>';
@@ -37,4 +46,4 @@
         table += '</table>';
         $('body').append(table);  
     }
-});
+  }
