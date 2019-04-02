d0cd53986434a8236ba9d541e648d50f9756d7c5
IngCampos <15151130@mail.ita.mx>
2019-04-02 11:54:00 -0600

    Estandarizacion de campos y eliminacion de nomenclaturas

--- js/script.js
+++ js/script.js
@@ -17,7 +17,70 @@
           }
           data2.push(a);
         }
-      
+
+        //Limpiar Hz
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][2] = data2[a][2].replace(/['HZ]+/g, '');
+          data2[a][2] = data2[a][2].replace(/[' ]+/g, '');
+        }
+        //Limpiar PPI, DPI, PPP
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][3] = data2[a][3].replace(/['DPI]+/g, '');
+          data2[a][3] = data2[a][3].replace(/['PPI]+/g, '');
+          data2[a][3] = data2[a][3].replace(/['PPP]+/g, '');
+          data2[a][3] = data2[a][3].replace(/[' ]+/g, '');
+        }
+        //Limpiar Resolucion
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][4] = data2[a][4].replace(/['X]+/g, '*');
+          data2[a][4] = data2[a][4].replace(/[' ]+/g, '');
+        }
+        //Limpiar Internal storage
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][6] = data2[a][6].replace(/[' ]+/g, '');
+          if(data2[a][6].indexOf("MB")!=-1){
+            data2[a][6] = data2[a][6].replace(/['MB]+/g, '');
+            data2[a][6] = (data2[a][6])/1024;
+            data2[a][6] = Math.round(data2[a][6]*10)/10;
+          }
+          else
+          data2[a][6] = data2[a][6].replace(/['GB]+/g, '');
+        }
+        //Limpiar Ram memory
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][7] = data2[a][7].replace(/[' ]+/g, '');
+          if(data2[a][7].indexOf("MB")!=-1){
+            data2[a][7] = data2[a][7].replace(/['MB]+/g, '');
+            data2[a][7] = (data2[a][7])/1024;
+            data2[a][7] = Math.round(data2[a][7]*10)/10;
+          }
+          else
+          data2[a][7] = data2[a][7].replace(/['GB]+/g, '');
+        }      
+        //Limpiar CPU speed
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][10] = data2[a][10].replace(/['MHZ]+/g, '');
+          data2[a][10] = data2[a][10].replace(/[' ]+/g, '');
+        }
+        //Limpiar main camera y front camera
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][11] = data2[a][11].replace(/['MP]+/g, '');
+          data2[a][11] = data2[a][11].replace(/['MPX]+/g, '');
+          data2[a][11] = data2[a][11].replace(/[' ]+/g, '');
+
+          data2[a][13] = data2[a][13].replace(/['MP]+/g, '');
+          data2[a][13] = data2[a][13].replace(/['MPX]+/g, '');
+          data2[a][13] = data2[a][13].replace(/[' ]+/g, '');
+        }
+        //Limpiar Battery capacity y voltaje
+        for (var a = 1; a < data2.length; a++) {
+          data2[a][18] = data2[a][18].replace(/['MAH]+/g, '');
+          data2[a][18] = data2[a][18].replace(/[' ]+/g, '');
+
+          data2[a][19] = data2[a][19].replace(/['V]+/g, '');
+          data2[a][19] = data2[a][19].replace(/[' ]+/g, '');
+        }
+        
         var table = '<table border=1px>';
         for (var singleRow = 0; singleRow < data2.length; singleRow++) {
           if (singleRow === 0) {
